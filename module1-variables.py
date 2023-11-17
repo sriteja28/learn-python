5342b434989ce9d287dbc5e599876f16192a449f
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 14:12:28 +0530

    Learning Python

---
+++ module1-variables.py
@@ -0,0 +1,11 @@
+greeting = 'Hello, friend'
+print(greeting)
+greeting = 'Hi, Everybody'
+print(greeting)
+
+# _correctName
+# correctName
+# correctName89
+# 89incorrect
+# variables can strt with _ or letters and not numbers, there are few reserved keywords
+
