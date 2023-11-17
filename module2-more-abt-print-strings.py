ff7db95d181616b257d2c504ba01915d613463c1
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 17:31:02 +0530

    Learning Python

---
+++ module2-more-abt-print-strings.py
@@ -0,0 +1,12 @@
+print(len('Hello'))
+
+print('Hello, World')
+print('Python speaking!')
+
+#Keyword arguments or named arguments
+print('Hello, World', end='.')
+print('Python speaking!')
+
+# sep character ,
+first_name = 'John'
+print('Your first name is', first_name, 'Welcome!', sep='-', end='=')
