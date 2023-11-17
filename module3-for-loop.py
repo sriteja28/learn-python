5b817b06e7c6ad3459947f203c0063a27523f4a5
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 20:37:08 +0530

    Learning Python

---
+++ module3-for-loop.py
@@ -0,0 +1,12 @@
+for letter in 'hello!':
+    print('Current letter:', letter)
+
+counter = 1
+while counter < 11:
+    print(counter)
+    counter += 1
+print('Finished')
+
+for counter in range(1, 11, 4):
+    print(counter)
+print('Finished')
