7285b1de3ebf074bfdb27580b273c5c2ee2cf21a
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 17:11:53 +0530

    Learning Python

---
+++ module2-type-casting.py
@@ -0,0 +1,14 @@
+height_cm = input('Height convertor: enter your heigt in cm: ')
+float_height_cm = float(height_cm)
+print('Your height in feet is:', float_height_cm / 30.48)
+
+
+height_cm = float(input('Height convertor: enter your heigt in cm: '))
+print('Your height in feet is:', height_cm / 30.48)
+
+height_cm = int(input('Height convertor: enter your heigt in cm: '))
+print('Your height in feet is:', height_cm / 30.48)
+
+float()
+int()
+str()
