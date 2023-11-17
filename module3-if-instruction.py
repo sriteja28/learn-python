e572fa55bc1ec3764dfc813b82872ac2acfc5aea
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 18:47:36 +0530

    Learning Python

---
+++ module3-if-instruction.py
@@ -0,0 +1,10 @@
+user_age = int(input('What is your age? '))
+if user_age > 30:
+    print('You more than 30 years old')
+    print('Sorry you are old')
+elif user_age == 30:
+    print('You are 30 years old')
+    print('you still have time')
+else:
+    print('You are 30 years old or younger')
+    print('you are kinda young')
