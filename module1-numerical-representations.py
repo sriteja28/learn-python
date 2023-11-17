d32ca1789155d50c4287ac73c7c3331fc70d7fa0
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 14:35:05 +0530

    Learning Python

---
+++ module1-numerical-representations.py
@@ -0,0 +1,15 @@
+#underscores are meaningless in numbers and can be used to make it easy to read
+12000300
+120_000_300
+
+#scientific notation
+# 3e4 = 3E4 = 3 * 10000 = 30000
+print(0.000000000000000000000000003)
+
+#octal numbers
+# start with 0o or Oo
+print(0o123)
+
+#hexadecimal numbers
+#strt with 0X or 0x
+print(0x123)
