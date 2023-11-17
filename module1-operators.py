c1d6c10d2f5c514f40fd26bbf0d3d9c94f4434cf
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 14:43:30 +0530

    Learning Python

---
+++ module1-operators.py
@@ -0,0 +1,20 @@
+print(2 + 3)
+print(5 - 4)
+print(5 * 4)
+
+# / Standard division, they always return float outputs
+print(6 / 2)
+print(7 / 2)
+
+# // integer division, they always return rounded outputs (lowest whole number)
+print(6 // 2)
+print(7 // 2)
+
+# % modulus division, returns the remainder of the division
+print(6 % 2)
+print(7 % 2)
+print(11 % 2)
+print(22 % 2)
+
+# ** power operator
+print(2 ** 3)
