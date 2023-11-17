bb147df181ee59410f1a0cf2acfac347730fe962
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 17:23:23 +0530

    Learning Python

---
+++ module2-more-abt-operators.py
@@ -0,0 +1,13 @@
+# Binary operators
+print(5 + 7)
+print(5 - 7)
+
+# unary and minus operators
+print(+12)
+print(-2) # - is number below 0, so it prints symbol
+
+# BODMAS
+print(2 + 3 * 2)
+
+# Exponential is read from right to left in Python
+print( 2 ** 2 ** 3)
