e69483365bfc947f212110e21d73ee58a7e052c1
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-17 15:34:10 +0530

    Learning Python

---
+++ module1-input.py
@@ -0,0 +1,14 @@
+
+# Functions in Python can do these two:
+# 1. Cause some effect
+# 2. Returns a value
+
+print('What is your name')
+user_name = input()
+print('Hello', user_name)
+
+
+user_name = input('What is your name')
+print('Hello', user_name)
+
+# input function always returns string even if we give number
