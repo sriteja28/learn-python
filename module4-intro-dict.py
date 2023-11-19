747e3c1d8835f9477c1a9630f3302ed2312d8510
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 16:52:14 +0530

    Learning Python

---
+++ module4-intro-dict.py
@@ -0,0 +1,11 @@
+# Collection used to store key value pairs - Dictonary
+
+emails = {
+    'Anne' : 'ash@email.com',
+    'Peter': 'peter@email.com',
+    'Mark' : 'mark@email.com',
+}
+
+print(emails['Mark'])
+
+# since list is mutable, it cannot be a key. Rest anythong can data type
