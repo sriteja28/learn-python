fb0342d56ef378cdbda8b91da3ffe693229af43a
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 16:06:52 +0530

    Learning Python

---
+++ module4-intro-tuples.py
@@ -0,0 +1,11 @@
+empty_tuple = ()
+one_el_tuple_a = (1,)
+one_el_tuple_b = 1,
+three_el_tuple = 1, 2, 3,
+print(three_el_tuple)
+
+# Tuples can have multiple datatypes
+# Lists have [] brackets, Tuples have () brackets
+# Main diffrence is Mutability
+# two kinds of data, mutable (can be updated, add new values like append) and immutable (you cannot add new values, only replace them)
+
