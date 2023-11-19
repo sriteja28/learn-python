156482c4f3171e4f2c5479cfb4dd1d43a2e8897d
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 19:28:01 +0530

    Learning Python

---
+++ module5-name-scopes.py
@@ -0,0 +1,9 @@
+def show_truth():
+    global mysterious_var
+    mysterious_var = 'New Surpirse'
+    print(mysterious_var)
+
+mysterious_var = 'Suprise'
+print(mysterious_var)
+show_truth()
+print(mysterious_var)
