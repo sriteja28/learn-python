48bf79546019997f5f815ca932b1ef8b8e52e21a
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 16:12:53 +0530

    Learning Python

---
+++ module4-tuple-operations.py
@@ -0,0 +1,16 @@
+user_data = ('John', 'American', 1964)
+print(len(user_data))
+
+user_data = ('John', 'American', 1964)
+if 'American' in user_data:
+    print('person from usa')
+
+user_data = ('John', 'American', 1964)
+for element in user_data:
+    print(element)
+
+user_data = ('John', 'American', 1964) + ('teacher', 'male')
+print(user_data)
+
+numbers = (0,1) * 10
+print(numbers)
