0f0f06277ee839035deae53c1507162a6e89f1b3
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 11:53:10 +0530

    Learning Python

---
+++ module4-intro-lists.py
@@ -0,0 +1,25 @@
+# Basic Datatypes are int, string, float, boolean
+# Complex Datatypes [Colletions] lists, tuples, dictionaries
+
+# Lists are used to store multiple values of same type, like multiple integers, multiple floats, multiple strings etc..
+
+empty_list = []
+top_cities = ['New York', 'Los Angles', 'Chicago', 'Houston', 'Phoenix']
+print(top_cities)
+
+# Indexing
+
+print(top_cities[0])
+print(top_cities[1])
+print(top_cities[4])
+print(top_cities[-1])
+print(top_cities[-2])
+print(top_cities[-5])
+
+#Slicing
+
+print(top_cities[0:2])
+print(top_cities[2:])
+print(top_cities[:3])
+print(top_cities[:])
+print(top_cities[10:15])
