4eafc915510a179af1add6c83a30f10d466daa0a
Sriteja Kattamuru <131655915+sriteja28@users.noreply.github.com>
2023-11-19 11:58:07 +0530

    Learning Python

---
+++ module4-delete-list.py
@@ -0,0 +1,12 @@
+top_cities = ['New York', 'Los Angles', 'Singapore', 'Chicago', 'Houston', 'Phoenix']
+del top_cities[2]
+print(top_cities)
+print(top_cities[2])
+del top_cities[3:]
+print(top_cities)
+del top_cities[:]
+print(top_cities)
+del top_cities
+print(top_cities)
+
+# del is not a function, its a instruction. diffrence is fucntion has a brackets, it doesnt
